3e2f85085f46e42accc9d10a4591cf7810f4639b
erikngigi <ericmosesngigi@gmail.com>
2026-01-03 14:01:32 +0300

    feat: update zsh config

--- zsh/.config/zsh/zsh-aliases.zsh
+++ zsh/.config/zsh/zsh-aliases.zsh
@@ -16,13 +16,10 @@ alias docker-volume-prune='docker volume rm $(docker volume ls)'
 alias docker-remove-dangling-images='docker rmi $(docker images -f "dangling=true" -q)'
 
 # Changing "ls" to "exa"
-alias ls='exa -al --color=always --group-directories-first' # my preferred listing
-alias la='exa -a --color=always --group-directories-first'  # all files and dirs
-alias ll='exa -l --color=always --group-directories-first'  # long format
-alias lt='exa -aT --color=always --group-directories-first' # tree listing
+alias la='exa -al --color=always --group-directories-first'  # all files and dirs
 
 # bat
-alias cat='bat'
+alias cat='batcat'
 
 # NvChad
 nvim_cd() {
