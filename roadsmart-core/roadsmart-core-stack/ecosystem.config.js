c35864376fafc07eabd7f58a27f38ef4ebcf3101
erikngigi <ericmosesngigi@gmail.com>
2026-01-19 17:44:57 +0300

    feat: add pm2 roadsmart configuration file to dotfiles

---
+++ roadsmart-core/roadsmart-core-stack/ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: "roadsmart-socket-service",
+      cwd: "/home/ubuntu/roadsmart-core-stack/roadsmart-socket-service",
+      script: "npm",
+      args: "start",
+      interpreter: "none",
+    },
+  ],
+};
