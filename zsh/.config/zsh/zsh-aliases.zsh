e85a36509b81b0451a736b83d3ede1cc73c645ee
erikngigi <ericmosesngigi@gmail.com>
2026-01-29 08:40:29 +0300

    feat: add aliases for advanced copy move

--- zsh/.config/zsh/zsh-aliases.zsh
+++ zsh/.config/zsh/zsh-aliases.zsh
@@ -16,24 +16,24 @@ alias docker-volume-prune='docker volume rm $(docker volume ls)'
 alias docker-remove-dangling-images='docker rmi $(docker images -f "dangling=true" -q)'
 
 # Changing "ls" to "exa"
-alias la='exa -al --color=always --group-directories-first'  # all files and dirs
+alias la='exa -al --color=always --group-directories-first' # all files and dirs
 
 # bat
 alias cat='batcat'
 
 # NvChad
 nvim_cd() {
-    local target_dir="${1:-.}"  # Default to current directory
-    
-    if [ ! -d "$target_dir" ]; then
-        echo "Error: Directory '$target_dir' not found"
-        return 1
-    fi
-    
-    local current_dir="$(pwd)"
-    cd "$target_dir" || return 1
-    nvim "$@"
-    cd "$current_dir"
+  local target_dir="${1:-.}" # Default to current directory
+
+  if [ ! -d "$target_dir" ]; then
+    echo "Error: Directory '$target_dir' not found"
+    return 1
+  fi
+
+  local current_dir="$(pwd)"
+  cd "$target_dir" || return 1
+  nvim "$@"
+  cd "$current_dir"
 }
 alias configrc='nvim_cd ~/.config/'
 alias nvimrc='nvim_cd ~/.config/nvim'
@@ -53,7 +53,7 @@ alias checkout='git checkout'
 alias clone='git clone'
 alias commit='git commit'
 alias fetch='git fetch'
-alias stat='git status'  # 'status' is protected name so using 'stat' instead
+alias stat='git status' # 'status' is protected name so using 'stat' instead
 alias tag='git tag'
 alias newtag='git tag -a'
 alias merge='git merge'
@@ -61,3 +61,7 @@ alias merge='git merge'
 # easier to read disk
 alias df='df -h'     # human-readable sizes
 alias free='free -m' # show sizes in MB
+
+# use advanced copy move
+alias cp='cpg'
+alias mv='mvg'
